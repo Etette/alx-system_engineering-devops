12cab3a26cac8ea5fbd28bc50e799c002953332c
Etette Etok <emmanueletok5@gmail.com>
2022-10-17 22:43:28 +0000

    README.md

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Use given REST API (for employer ID)
+return information about employer todo list
+progress
+"""
+
+import requests
+from sys import argv
+
+
+if __name__ = "__main__":
+    api = "https://jsonplaceholder.typicode.com/"
+    api_url_usr = api + "users/" + argv[1]
+    api_usr_todos = api + "todos?userId=" + argv[1]
+
+    response = requests.get(api_url_usr).json()
+    todo = resquests.get(api_url_todos).json()
+
+    complete_tasks = []
+    for task in todo:
+        if task.get("completed") is not False:
+            complete_tasks.append(task.get("title"))
+
+    print("Employer {} is done with tasks ({}/{}):".format(
+        response.get("name"), len(complete_tasks), len(todo)))
+    for title in complete_tasks:
+        print("\t {}".format(title))
