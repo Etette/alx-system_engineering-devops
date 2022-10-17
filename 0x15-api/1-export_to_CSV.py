7f8b36dfc7dadf518fb83c4f741f79e269ad74b9
Etette Etok <emmanueletok5@gmail.com>
2022-10-17 23:12:09 +0000

    1-export_to_CSV.py

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Use given REST API (for employer ID)
+return information about employer todo list
+progress. export data to CSV format
+"""
+
+import requests
+from sys import argv
+import csv
+
+
+if __name__ == "__main__":
+    api = "https://jsonplaceholder.typicode.com/"
+    api_url_usr = api + "users/" + argv[1]
+    api_url_todos = api + "todos?userId=" + argv[1]
+
+    response = requests.get(api_url_usr).json()
+    todo = requests.get(api_url_todos).json()
+
+    filename = argv[1] + ".CSV"
+    with open(filename, 'w', newline='') as csvfile:
+        write_file = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for task in todo:
+            write_file.writerow([int(argv[1]), response.get("username"),
+                                task.get("completed"), task.get("title")])
