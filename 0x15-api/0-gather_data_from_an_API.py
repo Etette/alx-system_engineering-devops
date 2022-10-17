b7c27d125c35858e7d2f73a95f3711b49bf891ab
Etette Etok <emmanueletok5@gmail.com>
2022-10-17 23:00:10 +0000

    fixed bug

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -12,10 +12,10 @@ from sys import argv
 if __name__ == "__main__":
     api = "https://jsonplaceholder.typicode.com/"
     api_url_usr = api + "users/" + argv[1]
-    api_usr_todos = api + "todos?userId=" + argv[1]
+    api_url_todos = api + "todos?userId=" + argv[1]
 
     response = requests.get(api_url_usr).json()
-    todo = resquests.get(api_url_todos).json()
+    todo = requests.get(api_url_todos).json()
 
     complete_tasks = []
     for task in todo:
