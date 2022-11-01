e6e55eaddd06781dc2f674ddea81029f9077960f
Etette Etok <emmanueletok5@gmail.com>
2022-11-01 17:20:19 +0000

    2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,39 +1,21 @@
 #!/usr/bin/python3
-"""
-Recursive function that queries the the REDDIT API
-"""
+"""returns a list containing the titles of all hot articles"""
 import requests
 
 
-def f_after(subreddit, hot_list=[], after=""):
-    """Recursive function to find length of hot_list
-    Args:
-        subreddit: Account to search
-        hot_list: hot list of reddit
-        after: next page of API
-    """
-    url = 'https://www.reddit.com/r/{}hot.json?limit=100'.format(subreddit)
-    if after is not None:
-        with requests.Session() as res:
-            headers = {'User-Agent': 'AgentMEGO'}
-            params = {'after': after}
-            data = res.get(url, headers, params)
-            if data.status_code != 200:
-                return None
-            after = data.json().get('data').get('after')
-        hot_list += data.json().get('data').get('children')
-        f_after(subreddit, hot_list, after)
-    return (hot_list)
-
-
 def recurse(subreddit, hot_list=[]):
-    """Returns a list containing titles of all hot articles for
-    a given subreddit.
-    Reruns None if no results are found for the given subreddit
-
-    Args:
-        sunreddit:  Account to search
-    """
-    if subreddit is None or type(subreddit) is not str:
+    headers = {'User-Agent': 'AgentMEGO'}
+    if len(hot_list) == 0:
+        resp = requests.get('https://www.reddit.com/r/{}/hot.json'
+                            .format(subreddit), headers=headers)
+    else:
+        resp = requests.get("https://www.reddit.com/r/{}/hot.json?after={}_{}"
+                            .format((subreddit), hot_list[-1].get('kind'),
+                                    hot_list[-1].get('data').get('id')),
+                            headers=headers)
+    if resp.status_code != requests.codes.OK:
         return None
-    return f_after(subreddit, hot_list, "")
+    if len(resp.json().get('data').get('children')) < 1:
+        return hot_list
+    hot_list.extend(resp.json().get('data').get('children'))
+    return recurse(subreddit, hot_list)
