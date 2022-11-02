355d7d53e68a617d9ab17551ca0c9f5263945e85
Etette Etok <emmanueletok5@gmail.com>
2022-11-02 08:51:56 +0000

    100-count.py

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""
+top Ten
+"""
+
+
+def count_words(subreddit, word_list, after="", word_dict{}):
+    """Returns a list of titles of all hot articles"""
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {'User-Agent': 'MyAPI/0.0.1'}
+    params = {'after': after}
+
+    if not word_dict:
+        for word in word_list:
+            word_dict[word] = 0
+
+    if after is None:
+        word_list = [[key, value] for key, value in word_dict.items()]
+        word_list = sorted(word_list, key=lambda x: (-x[1], x[0]))
+        for w in word_list:
+            if w[1]:
+                print("{}: {}".format(w[0].lower(), w[1]))
+            return None
+
+    r = requests.get(url, headers=headers, params=params,
+                     allow_redirects=False)
+    if r.status_code != 200:
+        return None
+
+    try:
+        js = r.json()
+    except ValueError:
+        return None
+
+    try:
+        data = js.get('data')
+        after = data.get('after')
+        children = data.get('children')
+        for child in children:
+            post = child.get('data')
+            title = post.get('title')
+            lower = [s.lower() for s in title.split(' ')]
+
+            for w in word_list:
+                word_dict[w] += lower.count(w.lower())
+
+    except Exception as e:
+        return None
+
+    count_words(subreddit, word_list, after, word_dict)
